54680b0964aeb57eed02999d0e4b3911502966a7
minkoo <ctyyc@naver.com>
2021-02-08 21:45:36 +0900

    first commit

---
+++ client/src/components/Config.js
@@ -0,0 +1,6 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const API_KEY = '61afc83b5c426a9b19d15d4b0ada1d9c';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/';
